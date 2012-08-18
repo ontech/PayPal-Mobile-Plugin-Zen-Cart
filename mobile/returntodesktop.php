5752ccecb07d182c297c45e347b829ba67b910da
mc12345678 <cm7a@cox.net>
2012-08-18 18:20:12 -0400

    Changed the term Return to Desktop Site (which would be applicable if was actually on a "Desktop" or perhaps understood elsewhere, but changed to Full Site because this Mobile version trims extra things off of it.

--- mobile/returntodesktop.php
+++ mobile/returntodesktop.php
@@ -1,3 +1,3 @@
 <div style="padding: 4px; width: 100%; text-align: center; font-size: 80%;">
-	<a href="#" onclick="document.cookie = 'showdesktop=1;'; window.location = './';" data-role="nojs">Return to Desktop site</a>
+	<a href="#" onclick="document.cookie = 'showdesktop=1;'; window.location = './';" data-role="nojs">Return to Full Site</a>
 </div>
