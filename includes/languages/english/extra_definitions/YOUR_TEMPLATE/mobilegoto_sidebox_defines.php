26c936f9b028b6dc922fbbd558985ef0e7278945
mc12345678 <cm7a@cox.net>
2012-08-18 21:46:51 -0400

    Files to accompany ReturnToMobile
    Oops... Somehow in the midst of things, the actual files that do/did work, didn't get incorporated.  Sorry.

---
+++ includes/languages/english/extra_definitions/YOUR_TEMPLATE/mobilegoto_sidebox_defines.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * mobilegoto language components - used when displaying components for going back to the mobile version of the website
+ *
+ * @package languageDefines
+ * @copyright Copyright 2003-2005 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: ez_pages_definitions.php 2739 2005-12-30 19:00:50Z birdbrain $
+ */
+
+
+// ez-pages sidebox title name
+define('BOX_HEADING_MOBILEGOTO_SIDEBOX','Return to the Mobile Version');
+
+?>
