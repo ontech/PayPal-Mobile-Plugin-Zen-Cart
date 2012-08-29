ea9d2065979599cd8df7b61b967f69165ea0cd3b
mc12345678 <cm7a@cox.net>
2012-08-29 01:27:03 -0400

    Adds the ability to process shipping after PayPal info obtained.
    Renamed files to be equivalent to what ZC uses (checkout_..., shopping_cart in place of cart)
    
    time_out and login do not have all the expected code within them needing more work
    
    Some of the other files do not have necessarily the correct "extra" text, but have functionality and default Zen Cart information.
    
    There's a few oddities in mobile.php: MatchGallery, MatchSearch, mobile_image, and some information at the end are just an example.

---
+++ mobile/login.php
@@ -0,0 +1,11 @@
+<!-- Need to program the login page which allows a new customer.-->
+
+<?php include 'header.php'; ?>
+
+<B>*Login*</B> function still needs to be fully developed.  At the moment, you have been brought to here, but may navigate away from it using the toolbar above or by going/continuing to the full site by the link below.<BR/>
+
+<?php include 'returntodesktop.php' ?>
+
+
+<?php include 'footer.php'; ?>
+
